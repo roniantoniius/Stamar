5cc5c232ba0624fea7e52d104ce537fb3a080b5b
roniantonius <roniantoniuss@upi.edu>
2024-11-01 15:00:17 +0700

    Kamis 31 Oct 2024 🎃: All visualization diagram all done in Web. --> Made KPI Cart with Function and For Loop --> Monthly Visualization

--- app/components/windrose.py
+++ app/components/windrose.py
@@ -3,49 +3,6 @@ import pandas as pd
 import plotly.express as px
 from app.models import DataFklim
 
-# def wind_rose(tahun=None, session=None):
-#     # Load data from the database
-#     df = session.query(DataFklim).all()
-#     df = pd.DataFrame([data.to_dict() for data in df])
-
-#     # Convert 'tahun' to an integer range based on input type
-#     if tahun is None:
-#         tahun_range = df['tahun'].unique()
-#     elif isinstance(tahun, str):
-#         tahun_range = [int(tahun)]
-#     elif isinstance(tahun, list) or isinstance(tahun, tuple):
-#         tahun_range = list(range(int(tahun[0]), int(tahun[1]) + 1))
-#     else:
-#         raise ValueError("Input tahun harus berupa string, list, atau tuple")
-
-#     # Filter data by year range and exclude rows with zero wind speed
-#     df_filtered = df[df['tahun'].isin(tahun_range)]
-#     df_filtered = df_filtered[df_filtered['anginkecmaks'] != 0]
-
-#     # Group by month and prepare data for each month
-#     wind_rose_data = {}
-#     for month in range(1, 13):
-#         monthly_data = df_filtered[df_filtered['bulan'] == month]
-#         if monthly_data.empty:
-#             continue
-
-#         # Aggregate by direction and calculate mean speed for each direction
-#         month_avg_data = monthly_data.groupby('anginarah')['anginkecmaks'].mean().reset_index()
-
-#         # Add data to the dictionary for each month
-#         wind_rose_data[calendar.month_abbr[month]] = {
-#             'labels': month_avg_data['anginarah'].tolist(),
-#             'datasets': [{
-#                 'label': f'Kecepatan Angin Rata-rata ({calendar.month_abbr[month]})',
-#                 'data': month_avg_data['anginkecmaks'].tolist(),
-#                 'backgroundColor': [
-#                     'rgba(255, 99, 132, 0.6)', 'rgba(54, 162, 235, 0.6)', 
-#                     'rgba(255, 206, 86, 0.6)', 'rgba(75, 192, 192, 0.6)',
-#                     'rgba(153, 102, 255, 0.6)', 'rgba(255, 159, 64, 0.6)'
-#                 ]
-#             }]
-#         }
-#     return wind_rose_data
 
 def wind_rose(tahun=None, session=None):
     df = session.query(DataFklim).all()
